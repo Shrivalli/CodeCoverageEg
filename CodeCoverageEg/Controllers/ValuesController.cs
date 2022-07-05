2ba370868ecceaeb8366045949b719f79cc32965
shrivalli04 <shrivalli04@gmail.com>
2022-07-05 15:36:23 +0530

    first commit

---
+++ CodeCoverageEg/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PipelineDemo.Models;
+using System.Linq;
+
+namespace PipelineDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<Employee> employees = Employee.getEmployees();
+
+        public ActionResult<Employee> getEmpById(int id)
+        {
+            Employee e=employees.Where(x=>x.Eid==id).SingleOrDefault();
+            return Ok(e);
+        }
+    }
+}
---
+++ CodeCoverageEg/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PipelineDemo.Models
+{
+    public class Employee
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public float Salary { get; set; }
+        public Employee() { }
+        public Employee(int eid,string ename,float sal)
+        {
+            Eid = eid;
+            Ename = ename;
+            Salary = sal;
+        }
+
+        public static List<Employee> emps = new List<Employee>();
+        public static List<Employee> getEmployees()
+        {
+            emps.Add(new Employee(1, "Shrivalli", 23090));
+            emps.Add(new Employee(2, "Shyam", 25000));
+            emps.Add(new Employee(3, "Rachna", 21000));
+            return emps;
+        }
+    }
+}
