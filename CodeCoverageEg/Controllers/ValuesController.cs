9171ee7baea7204542473451d186d694723d4099
shrivalli04 <shrivalli04@gmail.com>
2022-07-24 15:50:25 +0530

    commented other methods

--- CodeCoverageEg/Controllers/ValuesController.cs
+++ CodeCoverageEg/Controllers/ValuesController.cs
@@ -16,21 +16,21 @@ namespace CodeCoverageEg.Controllers
         {
             return Ok(employees);
         }
-        [HttpGet("{id}")]
-        public ActionResult<Employee> getEmpById(int id)
-        {
-            Employee e=employees.Where(x=>x.Eid==id).SingleOrDefault();
-            return Ok(e);
-        }
+        //[HttpGet("{id}")]
+        //public ActionResult<Employee> getEmpById(int id)
+        //{
+        //    Employee e=employees.Where(x=>x.Eid==id).SingleOrDefault();
+        //    return Ok(e);
+        //}
 
-        [HttpGet]
-        [Route("GetEmpSal")]
-        public ActionResult<int> getEmpBySal(int salmax)
-        {
-            var result=(from i in employees
-                       where i.Salary<salmax
-                       select i).ToList();
-            return Ok(result.Count);
-        }
+        //[HttpGet]
+        //[Route("GetEmpSal")]
+        //public ActionResult<int> getEmpBySal(int salmax)
+        //{
+        //    var result=(from i in employees
+        //               where i.Salary<salmax
+        //               select i).ToList();
+        //    return Ok(result.Count);
+        //}
     }
 }
